b0923570ac48bcbf2de43ead452aece8492de015
Alexandros Naskos <alex_naskos@hotmail.com>
2015-11-08 16:16:21 +0200

    More complicated graph

--- test/test.cpp
+++ test/test.cpp
@@ -43,18 +43,39 @@ int main(int argc, char *argv[]) {
 
     // Let's build something simple by hand
 
-    auto nodeA = graph.addNode({2, "A"});
+    auto nodeA = graph.addNode({3, "A"});
     auto nodeB = graph.addNode({4, "B"});
-    auto nodeC = graph.addNode({3, "C"});
-    auto nodeD = graph.addNode({5, "D"});
+    auto nodeC = graph.addNode({2, "C"});
+    auto nodeD = graph.addNode({1, "D"});
+    auto nodeE = graph.addNode({5, "E"});
+    auto nodeF = graph.addNode({3, "F"});
+    auto nodeG = graph.addNode({4, "G"});
+    auto nodeH = graph.addNode({3, "H"});
+    auto nodeI = graph.addNode({6, "I"});
 
     nodeA->earlyStart = 0;
 
-    nodeA->addArrow({ Arrow::Type::FS, 2, nodeA, nodeB });
-    nodeA->addArrow({ Arrow::Type::FF, -1, nodeA, nodeC });
+    nodeA->addArrow({ Arrow::Type::FF, 3, nodeA, nodeB });
+    nodeA->addArrow({ Arrow::Type::SS, 2, nodeA, nodeC });
+    nodeA->addArrow({ Arrow::Type::FS, 0, nodeA, nodeD });
 
-    nodeB->addArrow({ Arrow::Type::SS, 1, nodeB, nodeD });
-    nodeC->addArrow({ Arrow::Type::FS, 1, nodeC, nodeD });
+    nodeB->addArrow({ Arrow::Type::FS, 3, nodeB, nodeG });
+    nodeB->addArrow({ Arrow::Type::SF, -1, nodeB, nodeF });
+
+    nodeC->addArrow({ Arrow::Type::FS, 1, nodeC, nodeB });
+    nodeC->addArrow({ Arrow::Type::SF, 3, nodeC, nodeE });
+
+    nodeD->addArrow({ Arrow::Type::SS, 4, nodeD, nodeE });
+
+    nodeE->addArrow({ Arrow::Type::SS, 0, nodeE, nodeB });
+    nodeE->addArrow({ Arrow::Type::FF, 2, nodeE, nodeF });
+    nodeE->addArrow({ Arrow::Type::SF, 1, nodeE, nodeH });
+
+    nodeF->addArrow({ Arrow::Type::FF, 3, nodeF, nodeI });
+
+    nodeG->addArrow({ Arrow::Type::FS, 0, nodeG, nodeI });
+
+    nodeH->addArrow({ Arrow::Type::SS, -1, nodeH, nodeI });
 
     // This does nothing atm
     graph.checkGraph();
